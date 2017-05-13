62af45729e352c61c7a3280d8897c9c29a5e94b2
Arnold Farkas <farkasarnold1981@gmail.com>
2017-05-13 02:55:54 +0100

    indexing

--- src/main/scala/mosaic/Example.scala
+++ src/main/scala/mosaic/Example.scala
@@ -1,15 +1,32 @@
-import java.io.File
-import com.sksamuel.scrimage.Image
+import java.io.{File, PrintWriter}
+
 import ThumbnailMaker._
+import com.sksamuel.scrimage.Image
 
 object Example {
-  def main(args: Array[String]): Unit = {
-    val thumbnailSize = args(0).toInt
-    val inputFolder = new File(args(1))
-    val outputFolderPath = args(2)
 
-    inputFolder
-      .listFiles()
+  val extensions = List("jpg", "jpeg").map("." + _)
+
+  def main(args: Array[String]): Unit = {
+
+    args(0) match {
+      case "tiling" => tiling(args)
+      case "indexing" => indexing(args)
+    }
+  }
+
+  private def tiling(args: Array[String]): Unit = {
+    val thumbnailSize = args(1).toInt
+    val inputFolder = new File(args(2))
+    val outputFolderPath = args(3)
+
+    println(s"thumbnailSize: $thumbnailSize")
+    println(s"inputFolder: $inputFolder")
+    println(s"outputFolderPath: $outputFolderPath")
+
+    inputFolder.listFiles
+      .filter(_.isFile)
+      .filter(file => extensions.exists(file.getName.toLowerCase.endsWith(_)))
       .foreach(f => {
         val inputPath = f.getAbsolutePath
         val outputPath = outputFolderPath + f.getName
@@ -22,4 +39,25 @@ object Example {
         ).output(outputPath)
       })
   }
+
+  private def indexing(args: Array[String]) = {
+    val tileFolderPath = args(1)
+    val outputFilePath = args(2)
+
+    println(s"tileFolderPath: $tileFolderPath")
+    println(s"outputFilePath: $outputFilePath")
+
+    val csvRows = new File(tileFolderPath)
+      .listFiles()
+      .map(_.getAbsolutePath)
+      .map(path => (path, rgb(Image.fromFile(new File(path)))))
+
+    val output = csvRows.map(row => s"${row._1},${row._2.red},${row._2.green},${row._2.blue}").mkString("\n")
+
+    new PrintWriter(outputFilePath) {
+      write(output);
+      close
+    }
+  }
+
 }
--- src/main/scala/mosaic/ThumbnailMaker.scala
+++ src/main/scala/mosaic/ThumbnailMaker.scala
@@ -1,4 +1,4 @@
-import com.sksamuel.scrimage.Image
+import com.sksamuel.scrimage.{Color, Image}
 
 object ThumbnailMaker {
 
@@ -16,4 +16,31 @@ object ThumbnailMaker {
       .scaleTo(size, size)
   }
 
+  def rgb(image: Image): Color = {
+    val colors = for {
+      x <- 0 until image.width
+      y <- 0 until image.height
+    } yield image.color(x, y)
+
+    def avg(prevAvg: Double, count: Int, newVal: Double): Double = (prevAvg * count + newVal) / (count + 1)
+
+    colors
+      .map(c => (c.red.toDouble, c.green.toDouble, c.blue.toDouble))
+      .foldLeft[Option[(Int, (Double, Double, Double))]](None)(
+      (b, a) => {
+        b match {
+          case None => Some((1, a))
+          case Some((count, (r, g, b))) => Some((count + 1,
+            (
+              avg(r, count, a._1), avg(g, count, a._2), avg(b, count, a._3)
+            )
+          ))
+        }
+      }
+    )
+      .map(_._2)
+      .map(c => (c._1.toInt, c._2.toInt, c._3.toInt))
+      .map(c => Color(c._1, c._2, c._3))
+      .getOrElse(Color.Black)
+  }
 }
