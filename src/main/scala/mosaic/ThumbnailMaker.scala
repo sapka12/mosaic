f633b4ffcffdb1a6a393b3cb2eee07adf7b1bb4b
Arnold Farkas <farkasarnold1981@gmail.com>
2017-05-15 00:56:35 +0100

    base of segmentation, change and desegmentation

--- src/main/scala/mosaic/ThumbnailMaker.scala
+++ src/main/scala/mosaic/ThumbnailMaker.scala
@@ -1,6 +1,12 @@
-import com.sksamuel.scrimage.{Color, Image}
+import java.awt.image.BufferedImage
+
+import com.sksamuel.scrimage.{Color, Image, ImageMetadata}
+
+import scala.util.Random
 
 object ThumbnailMaker {
+
+  case class Tile(position: (Int, Int), image: Image)
 
   def thumbnail(image: Image, size: Int): Image = {
     val horizontal = image.width > image.height
@@ -42,5 +48,84 @@ object ThumbnailMaker {
       .map(c => (c._1.toInt, c._2.toInt, c._3.toInt))
       .map(c => Color(c._1, c._2, c._3))
       .getOrElse(Color.Black)
+  }
+
+  def segmentation(tileSize: Int, image: Image): Option[List[Tile]] = {
+
+    val maxPos: Option[(Int, Int)] = for {
+      img <- Some(image)
+      if (img.width % tileSize == 0)
+      if (img.height % tileSize == 0)
+      x <- img.width / tileSize
+      y <- img.width / tileSize
+    } yield (x, y)
+
+    val tiles: Option[(List[((Int, Int), Image)])] = maxPos.map(xy => {
+
+      val subImages = for {
+        x <- 0 to xy._1
+        y <- 0 to xy._2
+      } yield ((x, y), image.subimage(x * tileSize, y * tileSize, tileSize, tileSize))
+
+      subImages.toList
+    })
+
+    tiles.map(_.map(x => Tile(x._1, x._2)))
+    //      .map(randomize)
+  }
+
+  def randomize[A](as: List[A]): List[A] = {
+    def go(list: List[A], aggr: List[A]): List[A] =
+      if (list.isEmpty) aggr
+      else {
+        val randomIdx = Random.nextInt(list.size)
+        go(
+          list.take(randomIdx) ::: list.drop(randomIdx + 1),
+          aggr ::: List(list(randomIdx))
+        )
+      }
+
+    go(as, List())
+  }
+
+  //TODO remove tile duplication
+  def bestMatchStrategy(album: List[Tile])(tile: Tile): Tile = {
+
+    def sq(i: Int): Double = i.toDouble * i
+
+    def diff(c1: Color, c2: Color): Double = {
+      sq(c1.red - c2.red)
+      +sq(c1.green - c2.green)
+      +sq(c1.blue - c2.blue)
+    }
+
+    album.foldLeft(tile)((a, b) => {
+      val rgbTile = rgb(tile.image)
+      if (diff(rgbTile, rgb(a.image)) < diff(rgbTile, rgb(b.image))) a
+      else b
+    })
+  }
+
+  def change(tiles: List[Tile], strategy: Tile => Tile): List[Tile] =
+    tiles.map(strategy)
+
+  def bestMatchChange(input: Image, thumbnails: List[Tile], strategy: Tile => Tile): List[Tile] =
+    segmentation(64, input)
+      .map(tiles => change(tiles, bestMatchStrategy(thumbnails)))
+      .getOrElse(List())
+
+  def desegmentation(tiles: List[Tile]): Image = {
+    val maxHeight = tiles.map(t => (1 + t.image.height) * t.position._1).max
+    val maxWidth = tiles.map(t => (1 + t.image.width) * t.position._2).max
+
+    val baseImage = new Image(new BufferedImage(maxWidth, maxHeight, BufferedImage.TYPE_INT_ARGB), ImageMetadata(List()))
+
+    tiles.foldLeft(baseImage)((image, tile) => {
+      image.underlay(
+        tile.image,
+        tile.position._1 * tile.image.width,
+        tile.position._2 * tile.image.height
+      )
+    })
   }
 }
